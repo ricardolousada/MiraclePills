c46882bbfcaba5bf9d2a18deeaef85f581d5b13a
Ricardo Lousada <ricardo.lousada@gmail.com>
2017-09-23 09:46:44 +0100

    finalt commit

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -8,18 +8,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    
+    @IBOutlet weak var statePickerBtn: UIButton!
+
+    @IBOutlet weak var succssImage: UIImageView!
+    
+    let states = ["Alaska","Arkansas","Alabama","California","Maine","New Yourk"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //self.view.backgroundColor = UIColor.purple
+        statePicker.dataSource = self
+        statePicker.delegate = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    
 
+    @IBAction func StateButtonPressed(_ sender: Any) {
+        
+        statePicker.isHidden = false
+        //TAG flag with 1 to 4 will be hidden for picker to show
+        for index in 1...4 {
+            self.view.viewWithTag(index)?.isHidden = true
+        }
+    }
+    
+    
+    
+    @IBAction func BuyButtonPressed(_ sender: Any) {
+        
+        for view in self.view.subviews  {
+            view.isHidden = true
+        }
+        succssImage.isHidden = false
+        
+    }
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+        for index in 1...4 {
+            self.view.viewWithTag(index)?.isHidden = false
+        }
+
+    }
+    
 }
 
